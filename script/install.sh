250d8c4f13ac5303ab8b30ef18a4af96f02ce8a9
jbachelin <jordan.bachelin@gmail.com>
2016-11-17 15:25:16 +0100

    Update for new version of Solr (6.3.0)

--- script/install.sh
+++ script/install.sh
@@ -2,13 +2,13 @@
 
 #download solr
 echo "[info] Download solr ... "
-wget "http://wwwftp.ciril.fr/pub/apache/lucene/solr/6.2.1/solr-6.2.1.tgz"
+wget "http://wwwftp.ciril.fr/pub/apache/lucene/solr/6.3.0/solr-6.3.0.tgz"
 
 #unzip solr
-tar -xvf solr-6.2.1.tgz
+tar -xvf solr-6.3.0.tgz
 
 #put solr path in .bashrc
-echo "PATH=`pwd`/solr-6.2.1/bin:$PATH" > ~/.bashrc
+echo "PATH=`pwd`/solr-6.3.0/bin:$PATH" > ~/.bashrc
 
 source  ~/.bashrc
 echo "[info] Install apache solr with success. "
