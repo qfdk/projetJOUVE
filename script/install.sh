0bbb6ae92b2a73a8429a9cef3295dbe87ab4e9e9
kevin <kevin.ledy@gmail.com>
2016-11-17 16:06:25 +0100

    Refactoring script installation solr VERSION

--- script/install.sh
+++ script/install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+VERSION=6.3.0
 
 #download solr
 echo "[info] Download solr ... "
-wget "http://wwwftp.ciril.fr/pub/apache/lucene/solr/6.3.0/solr-6.3.0.tgz"
+wget "http://wwwftp.ciril.fr/pub/apache/lucene/solr/$VERSION/solr-$VERSION.tgz"
 
 #unzip solr
-tar -xvf solr-6.3.0.tgz
+tar -xvf solr-$VERSION.tgz
 
 #put solr path in .bashrc
-echo "PATH=`pwd`/solr-6.3.0/bin:$PATH" > ~/.bashrc
+echo "PATH=`pwd`/solr-$VERSION/bin:$PATH" > ~/.bashrc
 
 source  ~/.bashrc
 echo "[info] Install apache solr with success. "
