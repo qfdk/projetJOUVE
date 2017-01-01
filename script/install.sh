029529969fcbaa4f22d2d7e66e89d2e4c507fdb0
qfdk <qfdk@users.noreply.github.com>
2017-01-01 23:27:32 +0100

    [Fix] update

--- script/install.sh
+++ script/install.sh
@@ -6,7 +6,7 @@ echo "[info] Download solr ... "
 wget "http://wwwftp.ciril.fr/pub/apache/lucene/solr/$VERSION/solr-$VERSION.tgz"
 
 #unzip solr
-mkdir -P ~/bin
+mkdir -p ~/bin
 tar -xvf solr-$VERSION.tgz -C ~/bin
 
 #put solr path in .bashrc
