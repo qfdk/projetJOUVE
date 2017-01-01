7c08995b783660f25330349202e787ebc880d3b9
qfdk <qfdk@users.noreply.github.com>
2017-01-01 23:25:18 +0100

    [Fix] path

--- script/install.sh
+++ script/install.sh
@@ -6,11 +6,11 @@ echo "[info] Download solr ... "
 wget "http://wwwftp.ciril.fr/pub/apache/lucene/solr/$VERSION/solr-$VERSION.tgz"
 
 #unzip solr
-tar -xvf solr-$VERSION.tgz -C /usr/local/
+tar -xvf solr-$VERSION.tgz -C ~/bin
 
 #put solr path in .bashrc
 cd
-echo "PATH=/usr/local/solr-$VERSION/bin:$PATH" > .bashrc
+echo "PATH=~/bin/solr-$VERSION/bin:$PATH" > .bashrc
 source .bashrc
 
 echo "[info] Install apache solr with success. "
